71413764ca0b8d2c60a012dfa85c188c6aa6c1b0
Dylan Bailey <wearesleepless@gmail.com>
2022-10-25 11:19:50 -0400

    Adds quotes array

--- js/script.js
+++ js/script.js
@@ -10,7 +10,38 @@ project 1 - A Random Quote Generator
 /*** 
  * `quotes` array 
 ***/
-
+const quotes = [
+  {
+    quote: "Goodbye everyone, I'll remember you all in therapy.",
+    source: "Plankton",
+    citation: "The Algae's Always Greener",
+    year: 2001
+  },
+  {
+    quote: "Did you smell it? That smell. A kind of smelly smell. The smelly smell that smells...smelly.",
+    source: "Mr. Krabs",
+    citation: "Help Wanted",
+    year: 1999
+  },
+  {
+    quote: "The inner machinations of my mind are an enigma.",
+    source: "Patrick Star",
+    citation: "The Secret Box",
+    year: 2001
+  },
+  {
+    quote: "If I were to die right now in a fiery explosion due to the carelessness of a friend…. Then it would just be alright.",
+    source: "Spongebob Squarepants",
+    citation: "Dying For Pie",
+    year: 2000
+  },
+  {
+    quote: "This is not your average, everyday darkness. This is… ADVANCED darkness.",
+    source: "Spongebob Squarepants",
+    citation: "Rock Bottom",
+    year: 2000
+  },
+];
 
 
 /***
